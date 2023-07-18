14d86dab86dab14743d531df9a19d4d5a097ff43
Jean Victor <jeamsillva3@gmail.com>
2023-07-18 17:50:01 -0300

    status change

--- src/data/MyProjects.ts
+++ src/data/MyProjects.ts
@@ -1,34 +1,3 @@
-/* export const myProjects = [
-  {
-    id: 1,
-    tags: "Web",
-    name: "Coffee Delivery",
-    photo: "https://github.com/DJeanS03/ToDo_app/assets/109162543/cbb902b2-f526-47f9-b0fb-82f368504a1c",
-    link: "https://coffee-delivery-ignite-chi.vercel.app",
-    status: "In progress",
-    state: "in__progress",
-  },
-  {
-    id: 2,
-    tags: "Web",
-    name: "Pomodo Timer",
-    photo: "https://github.com/DJeanS03/DJeanS03/assets/109162543/b0bd2663-63f6-4023-aa6b-5e81a60eb4a1",
-    link: "https://pomodoro-timer-pearl-eta.vercel.app",
-    status: "Concluded",
-    state: "concluded",
-  },
-  {
-    id: 3,
-    tags: "Web",
-    name: "ToDo app",
-    photo: "https://github.com/DJeanS03/ToDo_app/assets/109162543/d37059c6-3ae9-4915-9a6e-2f2e18e0a7b5",
-    link: "https://todo-app-xi-ruby.vercel.app",
-    status: "Concluded",
-    state: "concluded",
-  },
-];
- */
-
 export const myProjects = [
   {
     id: 1,
@@ -36,16 +5,16 @@ export const myProjects = [
     name: "Coffee Delivery",
     photo: "https://github.com/DJeanS03/ToDo_app/assets/109162543/cbb902b2-f526-47f9-b0fb-82f368504a1c",
     link: "https://coffee-delivery-ignite-chi.vercel.app",
-    status: "In progress",
-    state: "in__progress",
+    status: "Concluded",
+    state: "concluded",
     translations: {
       en: {
         name: "Coffee Delivery",
-        status: "In progress",
+        status: "Concluded",
       },
       pt: {
         name: "Entrega de Café",
-        status: "Em andamento",
+        status: "Concluído",
       },
     },
   },
