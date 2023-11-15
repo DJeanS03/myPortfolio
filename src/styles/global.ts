4d8fc000c02fe9309ba2c6fbbe1644651f7f5563
DJeanS03 <jeamsillva3@gmail.com>
2023-11-15 11:13:09 -0300

    remove part area

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,7 +7,7 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
-@media screen and (min-width: 968px) {
+/* @media screen and (min-width: 968px) {
     :root {
         --biggest-font-size: 4rem;
         --bigger-font-size: 3rem;
@@ -18,7 +18,7 @@ export const GlobalStyle = createGlobalStyle`
         --small-font-size: .875rem;
         --smaller-font-size: .813rem;
     }
-}
+} */
 
 html {
     scroll-behavior: smooth;
